94dd8f40ecd558896a959b20fc9011c920f2124a
Benoit <guiraudou@osimatic.com>
2022-05-09 18:31:30 +0200

    ajout logs

--- android_helpers/src/main/java/com/osimatic/android_helpers/ProgressDialog.java
+++ android_helpers/src/main/java/com/osimatic/android_helpers/ProgressDialog.java
@@ -1,10 +1,14 @@
 package com.osimatic.android_helpers;
 
 import android.app.Activity;
+import android.util.Log;
 
 public class ProgressDialog {
+	private static final String TAG = Config.START_TAG+"ProgressDialog";
+
 	public static android.app.ProgressDialog getProgressDialog(final Activity activity) {
 		if (null == activity) {
+			Log.e(TAG, "activity null");
 			return null;
 		}
 		android.app.ProgressDialog progressDialog = new android.app.ProgressDialog(activity);
@@ -15,7 +19,12 @@ public class ProgressDialog {
 	}
 
 	public static void showProgressDialog(final Activity activity, android.app.ProgressDialog progressDialog) {
-		if (null == activity || null == progressDialog) {
+		if (null == activity) {
+			Log.e(TAG, "activity null");
+			return;
+		}
+		if (null == progressDialog) {
+			Log.e(TAG, "progressDialog null");
 			return;
 		}
 		activity.runOnUiThread(new Runnable() {
@@ -28,7 +37,12 @@ public class ProgressDialog {
 	}
 
 	public static void hideProgressDialog(final Activity activity, android.app.ProgressDialog progressDialog) {
-		if (null == activity || null == progressDialog) {
+		if (null == activity) {
+			Log.e(TAG, "activity null");
+			return;
+		}
+		if (null == progressDialog) {
+			Log.e(TAG, "progressDialog null");
 			return;
 		}
 		activity.runOnUiThread(new Runnable() {
